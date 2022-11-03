9e710dd366d43c879aca54530a576cacd3e4f5fd
bitgraf <ablaskov27@gmail.com>
2022-11-03 18:52:35 -0500

    test GLM

--- main.cpp
+++ main.cpp
@@ -9,6 +9,26 @@
 #include "imgui_impl_glfw.h"
 #include <imgui_impl_opengl3.h>
 
+#define GLM_FORCE_MESSAGES
+#include <glm/glm.hpp>
+
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+//#include <glm/ext/constants.hpp> // glm::pi
+
+glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
+{
+    glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+    glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+    View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+    View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+    return Projection * View * Model;
+}
+
 static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
@@ -63,6 +83,9 @@ int main(void)
 
     // Our state
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Test glm functions
+    camera(0, glm::vec2(0, 0));
 
     while (!glfwWindowShouldClose(window))
     {
